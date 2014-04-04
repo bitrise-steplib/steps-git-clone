8caeabc752253d3418bdf89c380da80336f37913
Barnabás Birmacher <birmacher@gmail.com>
2014-04-04 23:21:52 +0200

    ability to clone only the desired branch

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Clones a specified git repository to the desired relative path
 
 ## Environment variables
 GIT_REPOSITORY_URL - the git repository you want to clone
+GIT_BRANCH - optional; Set only if you want to clone just that branch
--- step.sh
+++ step.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+if [ ! -n "$GIT_BRANCH" ]; then
+  export GIT_BRANCH_PARAMETER="--no-single-branch"
+else
+  export GIT_BRANCH_PARAMETER="--single-branch --branch $GIT_BRANCH"
+fi
+
 # Clone the repository
-git clone --quiet --recursive --no-single-branch $GIT_REPOSITORY_URL $CONCRETE_RELATIVE_GIT_DIRECTORY
+git clone --recursive $GIT_BRANCH_PARAMETER $GIT_REPOSITORY_URL $CONCRETE_RELATIVE_GIT_DIRECTORY
