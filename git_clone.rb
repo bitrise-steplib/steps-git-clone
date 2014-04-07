39a44a456053bb503255063d5f902d555a2cc704
Viktor <viktor>
2014-04-07 14:37:59 +0200

    ssh-private-key through base64 encoding and username+psw support - through environment variables

--- git_clone.rb
+++ git_clone.rb
@@ -1,5 +1,6 @@
 require 'base64'
 require 'fileutils'
+require 'uri'
 
 options = {
   GIT_BRANCH: ENV['GIT_BRANCH'],
@@ -15,13 +16,68 @@ options = {
 
 p "options: #{options}"
 
-if options[:AUTH_SSH_PRIVATE_KEY_BASE64]
-  private_key_file_path = File.join(options[:USER_HOME], '.ssh/id_rsa')
+
+def write_private_key_to_file(user_home, auth_ssh_private_key_base64)
+  private_key_file_path = File.join(user_home, '.ssh/id_rsa')
   p "private_key_file_path: #{private_key_file_path}"
   # create the folder if not yet created
-  FileUtils::mkdir_p(File.join(options[:USER_HOME], '.ssh'))
+  FileUtils::mkdir_p(File.join(user_home, '.ssh'))
   # private key - save to file
-  private_key_decoded = Base64.decode64(options[:AUTH_SSH_PRIVATE_KEY_BASE64])
+  private_key_decoded = Base64.decode64(auth_ssh_private_key_base64)
   File.open(private_key_file_path, 'wt') { |f| f.write(private_key_decoded) }
   system "chmod 600 #{private_key_file_path}"
+  p "Private key written to file."
 end
+
+def add_host_to_known_hosts_if_needed(repo_url)
+  host_regex = /@[a-z]*\.[a-z]*/
+  match_res = host_regex.match(repo_url)
+  unless match_res
+    p "[!] Host can't be determined from url: #{repo_url}"
+    return false
+  end
+  host = match_res[0][1..-1] # remove the @ from the beginning of the host url
+  p "Host url: #{host}"
+  system "ssh -o StrictHostKeyChecking=no #{host}"
+  return true
+end
+
+prepared_repository_url = options[:GIT_REPOSITORY_URL]
+
+if options[:AUTH_SSH_PRIVATE_KEY_BASE64] and options[:AUTH_SSH_PRIVATE_KEY_BASE64].length > 0
+  p "[i] Auth: using SSH private key (base64)"
+  write_private_key_to_file(options[:USER_HOME], options[:AUTH_SSH_PRIVATE_KEY_BASE64])
+  add_host_to_known_hosts_if_needed(prepared_repository_url)
+elsif options[:AUTH_USER] and options[:AUTH_USER].length > 0 and options[:AUTH_PASSWORD] and options[:AUTH_PASSWORD].length > 0
+  p "[i] Auth: with username and password"
+  # https://viktorbenei@bitbucket.org/concrete-team/step-environment-writer.git
+  repo_prefix_regex = /^https?:\/\/[a-z]*@/
+  rres = repo_prefix_regex.match(prepared_repository_url)
+  unless rres
+    p "[!] Invalid url prefix: should start with 'http(s)://...@'"
+  else
+    http_part = /^https?:\/\//.match(prepared_repository_url)[0]
+    # strip out the "prefix" part
+    prepared_repository_url = prepared_repository_url[rres[0].length .. -1]
+    # and recunstruct, with the auth parameters
+    prepared_repository_url = "#{http_part}#{options[:AUTH_USER]}:#{options[:AUTH_PASSWORD]}@#{prepared_repository_url}"
+  end
+else
+  p "[i] Auth: No Authentication information found - trying without authentication"
+end
+
+# do clone
+p "prepared_repository_url: #{prepared_repository_url}"
+
+git_branch_parameter = ""
+if options[:GIT_BRANCH] and options[:GIT_BRANCH].length > 0
+  git_branch_parameter = "--single-branch --branch #{options[:GIT_BRANCH]}"
+else
+  git_branch_parameter = "--no-single-branch"
+end
+
+full_git_clone_command_string = "git clone --recursive #{git_branch_parameter} #{prepared_repository_url} #{options[:CLONE_DESTINATION_DIR]}"
+p "full_git_clone_command_string: #{full_git_clone_command_string}"
+# system full_git_clone_command_string
+
+
--- step.sh
+++ step.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-if [ ! -n "$GIT_BRANCH" ]; then
-  export GIT_BRANCH_PARAMETER="--no-single-branch"
-else
-  export GIT_BRANCH_PARAMETER="--single-branch --branch $GIT_BRANCH"
-fi
+# if [ ! -n "$GIT_BRANCH" ]; then
+#   export GIT_BRANCH_PARAMETER="--no-single-branch"
+# else
+#   export GIT_BRANCH_PARAMETER="--single-branch --branch $GIT_BRANCH"
+# fi
+
+# ssh -o StrictHostKeyChecking=no bitbucket.org/github.com -> to add host to known_hosts
 
 # Clone the repository
 # git clone --recursive $GIT_BRANCH_PARAMETER $GIT_REPOSITORY_URL $CONCRETE_RELATIVE_GIT_DIRECTORY
