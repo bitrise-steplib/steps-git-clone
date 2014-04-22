bc86881e444c5beb45fa6ce1e6ea5d85bf259eae
Viktor <viktor>
2014-04-22 15:23:24 +0200

    removed previous "known host / ssh" prompt suppress method, it's now part of the GIT_SSH script (ssh_no_prompt.sh) + removed "system export"s because those DON'T effect subsequent system calls!

--- git_clone.rb
+++ git_clone.rb
@@ -68,26 +68,6 @@ def write_private_key_to_file(user_home, auth_ssh_private_key_base64)
 end
 
 
-def add_host_to_known_hosts_if_needed(repo_url)
-  #
-  # regex based because it's used for repos with url like this: git@bitbucket.org:...-team/...-writer.git
-  #  Ruby uri module doesn't support urls like this
-  #
-
-  host_regex = /@[a-z]*\.[a-z]*/
-  match_res = host_regex.match(repo_url)
-  unless match_res
-    p "[!] Host can't be determined from url: #{repo_url}"
-    return false
-  end
-  host = match_res[0][1..-1] # remove the @ from the beginning of the host url
-
-  system "ssh -o StrictHostKeyChecking=no -o BatchMode=yes #{host}"
-  return true
-end
-
-
-
 # -----------------------
 # --- main
 # -----------------------
@@ -95,19 +75,8 @@ end
 #
 prepared_repository_url = options[:repo_url]
 
-begin
-  URI.parse(prepared_repository_url)
-rescue
-  # probably an git ssh url like git@bitbucket.org:...
-  # suppress known-host prompt
-  add_host_to_known_hosts_if_needed(prepared_repository_url)
-end
-
 if options[:auth_ssh_key_base64] and options[:auth_ssh_key_base64].length > 0
   write_private_key_to_file(options[:user_home], options[:auth_ssh_key_base64])
-
-  # suppress known-host prompt
-  add_host_to_known_hosts_if_needed(prepared_repository_url)
 elsif options[:auth_username] and options[:auth_username].length > 0 and options[:auth_password] and options[:auth_password].length > 0
   repo_uri = URI.parse(prepared_repository_url)
   
@@ -130,9 +99,7 @@ end
 # GIT_ASKPASS=echo : this will automatically fail if git would show a password prompt
 full_git_clone_command_string = "git clone --recursive #{git_branch_parameter} #{prepared_repository_url} #{options[:clone_destination_dir]}"
 
-system "export GIT_ASKPASS=echo"
-system 'export GIT_SSH="$PWD/ssh_no_prompt.sh"'
 puts "$ #{full_git_clone_command_string}"
-system full_git_clone_command_string
+system 'GIT_ASKPASS=echo GIT_SSH="./ssh_no_prompt.sh ' + full_git_clone_command_string
 
 
