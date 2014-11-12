339b471bef2a4cdc71b025896ab2b3a767db4a35
Viktor Benei <viktor.benei@gmail.com>
2014-11-12 20:56:51 +0000

    step sh style fix

--- step.sh
+++ step.sh
@@ -20,14 +20,14 @@ fi
 echo " (i) is_export_outputs: ${is_export_outputs}"
 
 ruby ./git_clone.rb \
-	--repo-url=${GIT_REPOSITORY_URL} \
-	--commit-hash=${BITRISE_GIT_COMMIT} \
-	--tag=${BITRISE_GIT_TAG} \
-	--branch=${BITRISE_GIT_BRANCH} \
-	--dest-dir=${BITRISE_SOURCE_DIR} \
-	--auth-username=${AUTH_USER} \
-	--auth-password=${AUTH_PASSWORD} \
-	--auth-ssh-base64=${AUTH_SSH_PRIVATE_KEY_BASE64} \
+	--repo-url="${GIT_REPOSITORY_URL}" \
+	--commit-hash="${BITRISE_GIT_COMMIT}" \
+	--tag="${BITRISE_GIT_TAG}" \
+	--branch="${BITRISE_GIT_BRANCH}" \
+	--dest-dir="${BITRISE_SOURCE_DIR}" \
+	--auth-username="${AUTH_USER}" \
+	--auth-password="${AUTH_PASSWORD}" \
+	--auth-ssh-base64="${AUTH_SSH_PRIVATE_KEY_BASE64}" \
 	--formatted-output-file="${formatted_output_file_path}" \
 	--is-export-outputs="${is_export_outputs}"
 
