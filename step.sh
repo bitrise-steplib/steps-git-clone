81f74274ff4aca03b8b80e2efb1861610f1b0896
Viktor Benei <viktor.benei@gmail.com>
2016-08-01 09:56:12 +0200

    enable `GO15VENDOREXPERIMENT` for Go 1.5

--- step.sh
+++ step.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export GO15VENDOREXPERIMENT=1
 GOPATH="$THIS_SCRIPT_DIR/go" go run "${THIS_SCRIPT_DIR}/go/src/github.com/bitrise-io/git-clone/main.go"
 exit $?
 
