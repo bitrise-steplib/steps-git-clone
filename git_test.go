0684002709d07a3ee3be3c3eb81a863aeaa2cd62
Krisztian Dobmayer <krisztian.dobmayer@bitrise.io>
2018-01-26 16:37:41 +0100

    Remove one test case

--- git_test.go
+++ git_test.go
@@ -4,9 +4,8 @@ import "testing"
 
 func TestFetchArg(t *testing.T) {
 	for input, expected := range map[string]string{
-		"pull/1/merge":   "pull/1/head:pull/1",
-		"pull/22/merge":  "pull/22/head:pull/22",
-		"pull/333/merge": "pull/333/head:pull/333",
+		"pull/1/merge":  "pull/1/head:pull/1",
+		"pull/22/merge": "pull/22/head:pull/22",
 	} {
 		actual := fetchArg(input)
 		if actual != expected {
