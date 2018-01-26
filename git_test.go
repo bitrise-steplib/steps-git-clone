e66a3f84c7eef95bd3d7793731c3f31a1d1cf54d
Krisztian Dobmayer <krisztian.dobmayer@bitrise.io>
2018-01-26 16:34:24 +0100

    Update tests

--- git_test.go
+++ git_test.go
@@ -1,28 +1,16 @@
 package main
 
-import (
-	"testing"
-)
-
-var testCases = []struct {
-	input    string
-	expected string
-}{
-	{
-		input:    "pull/1/merge",
-		expected: "pull/1/head:pull/1",
-	},
-	{
-		input:    "pull/22/merge",
-		expected: "pull/22/head:pull/22",
-	},
-}
+import "testing"
 
 func TestFetchArg(t *testing.T) {
-	for _, test := range testCases {
-		actual := fetchArg(test.input)
-		if actual != test.expected {
-			t.Errorf("fetchArg(%q), expected %q, actual %q", test.input, test.expected, actual)
+	for input, expected := range map[string]string{
+		"pull/1/merge":   "pull/1/head:pull/1",
+		"pull/22/merge":  "pull/22/head:pull/22",
+		"pull/333/merge": "pull/333/head:pull/333",
+	} {
+		actual := fetchArg(input)
+		if actual != expected {
+			t.Errorf("fetchArg(%q), expected %q, actual %q", input, expected, actual)
 		}
 	}
 }
