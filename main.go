6296e42684c5139c7972961d958550938fab7be0
DamienBitrise <60897705+DamienBitrise@users.noreply.github.com>
2020-08-26 23:47:54 -0700

    Update main.go

--- main.go
+++ main.go
@@ -127,7 +127,6 @@ func mainE() error {
 			return fmt.Errorf("checkout (%s): %v", checkoutArg, err)
 		}
 		// Update branch: 'git fetch' followed by a 'git merge' is the same as 'git pull'.
-		log.Printf("ZZZ cfg.AutoMerge")
 		if checkoutArg == cfg.Branch && cfg.AutoMerge{
 			if err := run(gitCmd.Merge("origin/" + cfg.Branch)); err != nil {
 				return fmt.Errorf("merge %q: %v", cfg.Branch, err)
