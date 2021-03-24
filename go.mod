3e79df8ee0dde247853e4ca0129715d9822f83f6
Adam Borbas <adam.borbas@bitrise.io>
2021-03-24 09:25:43 +0100

    Update mods

--- go.mod
+++ go.mod
@@ -5,11 +5,11 @@ go 1.16
 require (
 	github.com/bitrise-io/bitrise-init v0.0.0-20201117094539-a984f01dd477
 	github.com/bitrise-io/envman v0.0.0-20200512105748-919e33f391ee
-	github.com/bitrise-io/go-steputils v0.0.0-20201016102104-03ae3a6ded35
-	github.com/bitrise-io/go-utils v0.0.0-20210316133228-449620935158
+	github.com/bitrise-io/go-steputils v0.0.0-20210324082442-21a1b8f2b237
+	github.com/bitrise-io/go-utils v0.0.0-20210323091856-00429d8e1e87
 	github.com/bitrise-io/goinp v0.0.0-20190611131639-bd18a8681e27 // indirect
 	github.com/bitrise-steplib/bitrise-step-export-universal-apk v0.0.0-20200729103519-a582681d23d6
-	github.com/stretchr/testify v1.5.1
+	github.com/stretchr/testify v1.6.1
 	golang.org/x/crypto v0.0.0-20201116153603-4be66e5b6582 // indirect
 	golang.org/x/sys v0.0.0-20201116194326-cc9327a14d48 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
--- vendor/github.com/bitrise-io/go-utils/command/git/commands.go
+++ vendor/github.com/bitrise-io/go-utils/command/git/commands.go
@@ -62,8 +62,9 @@ func (g *Git) Clean(options ...string) *command.Model {
 }
 
 // SubmoduleUpdate updates the registered submodules.
-func (g *Git) SubmoduleUpdate(shallowCheckout bool) *command.Model {
+func (g *Git) SubmoduleUpdate(shallowCheckout bool, opts ...string) *command.Model {
 	args := []string{"submodule", "update", "--init", "--recursive"}
+	args = append(args, opts...)
 	if shallowCheckout {
 		args = append(args, "--depth=1")
 	}
@@ -142,4 +143,20 @@ func (g *Git) Status(opts ...string) *command.Model {
 // Config sets a git config setting for the repository.
 func (g *Git) Config(key string, value string) *command.Model {
 	return g.command("config", key, value)
+}
+
+// SparseCheckoutInit initializes the sparse-checkout config file.
+func (g *Git) SparseCheckoutInit(cone bool) *command.Model {
+	args := []string{"sparse-checkout", "init"}
+	if cone {
+		args = append(args, "--cone")
+	}
+	return g.command(args...)
+}
+
+// SparseCheckoutSet writes the provided patterns to the sparse-checkout config file.
+func (g *Git) SparseCheckoutSet(opts ...string) *command.Model {
+	args := []string{"sparse-checkout", "set"}
+	args = append(args, opts...)
+	return g.command(args...)
 }
