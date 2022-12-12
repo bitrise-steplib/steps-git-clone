e17134aa261456e7859fac735e2e201d29b553a6
Olivér Falvai <oliver.falvai@bitrise.io>
2022-12-12 18:01:34 +0100

    Use v2 stepconf parser (#189)

--- main.go
+++ main.go
@@ -4,8 +4,9 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/bitrise-io/go-steputils/stepconf"
+	"github.com/bitrise-io/go-steputils/v2/stepconf"
 	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-io/go-utils/v2/env"
 	"github.com/bitrise-steplib/steps-git-clone/gitclone"
 )
 
@@ -16,7 +17,8 @@ func failf(format string, v ...interface{}) {
 
 func main() {
 	var cfg gitclone.Config
-	if err := stepconf.Parse(&cfg); err != nil {
+	envRepo := env.NewRepository()
+	if err := stepconf.NewInputParser(envRepo).Parse(&cfg); err != nil {
 		failf("Error: %s\n", err)
 	}
 	stepconf.Print(cfg)
