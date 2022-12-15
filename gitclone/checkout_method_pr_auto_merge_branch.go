dd839cfcc4879da479c3727490b2e9110e7725a9
Olivér Falvai <oliver.falvai@bitrise.io>
2022-12-15 10:12:43 +0100

    Use PR head ref in output export

--- gitclone/checkout_method_pr_auto_merge_branch.go
+++ gitclone/checkout_method_pr_auto_merge_branch.go
@@ -14,20 +14,26 @@ type PRMergeRefParams struct {
 	DestinationBranch string
 	// MergeRef contains the changes pre-merged by the git provider (eg. pull/7/merge)
 	MergeRef string
+	// HeadRef is the head of the PR branch (eg. pull/7/head)
+	HeadRef string
 }
 
 // NewPRMergeRefParams validates and returns a new PRMergeRefParams
-func NewPRMergeRefParams(destBranch, mergeRef string) (*PRMergeRefParams, error) {
+func NewPRMergeRefParams(destBranch, mergeRef, headRef string) (*PRMergeRefParams, error) {
 	if strings.TrimSpace(destBranch) == "" {
 		return nil, NewParameterValidationError("Can't checkout PR: no destination branch specified")
 	}
 	if strings.TrimSpace(mergeRef) == "" {
 		return nil, NewParameterValidationError("Can't checkout PR: no merge ref specified")
 	}
+	if strings.TrimSpace(headRef) == "" {
+		return nil, NewParameterValidationError("Can't checkout PR: no head ref specified")
+	}
 
 	return &PRMergeRefParams{
 		DestinationBranch: destBranch,
 		MergeRef:          mergeRef,
+		HeadRef:           headRef,
 	}, nil
 }
 
@@ -37,7 +43,7 @@ type checkoutPRMergeRef struct {
 
 func (c checkoutPRMergeRef) do(gitCmd git.Git, fetchOpts fetchOptions, fallback fallbackRetry) error {
 	// https://git-scm.com/book/en/v2/Git-Internals-The-Refspec
-	refSpec := fmt.Sprintf("%s:%s", c.remoteRef(), c.localRef())
+	refSpec := fmt.Sprintf("%s:%s", c.remoteMergeRef(), c.localMergeRef())
 
 	// $ git fetch origin refs/remotes/pull/7/merge:refs/pull/7/merge
 	err := fetch(gitCmd, originRemoteName, refSpec, fetchOpts)
@@ -45,8 +51,15 @@ func (c checkoutPRMergeRef) do(gitCmd git.Git, fetchOpts fetchOptions, fallback 
 		return err
 	}
 
+	// Also fetch the PR head ref because the step exports outputs based on the PR head commit (see output.go)
+	// $ git fetch origin refs/remotes/pull/7/head:refs/pull/7/head
+	err = c.fetchPRHeadRef(gitCmd, fetchOpts)
+	if err != nil {
+		return err
+	}
+
 	// $ git checkout refs/remotes/pull/7/merge
-	err = checkoutWithCustomRetry(gitCmd, c.localRef(), nil)
+	err = checkoutWithCustomRetry(gitCmd, c.localMergeRef(), nil)
 	if err != nil {
 		return err
 	}
@@ -55,13 +68,26 @@ func (c checkoutPRMergeRef) do(gitCmd git.Git, fetchOpts fetchOptions, fallback 
 }
 
 func (c checkoutPRMergeRef) getBuildTriggerRef() string {
-	return c.localRef()
+	return c.localHeadRef()
 }
 
-func (c checkoutPRMergeRef) localRef() string {
+func (c checkoutPRMergeRef) localMergeRef() string {
 	return fmt.Sprintf("refs/remotes/%s", c.params.MergeRef)
 }
 
-func (c checkoutPRMergeRef) remoteRef() string {
+func (c checkoutPRMergeRef) remoteMergeRef() string {
 	return fmt.Sprintf("refs/%s", c.params.MergeRef)
+}
+
+func (c checkoutPRMergeRef) localHeadRef() string {
+	return fmt.Sprintf("refs/remotes/%s", c.params.HeadRef)
+}
+
+func (c checkoutPRMergeRef) remoteHeadRef() string {
+	return fmt.Sprintf("refs/%s", c.params.HeadRef)
+}
+
+func (c checkoutPRMergeRef) fetchPRHeadRef(gitCmd git.Git, fetchOpts fetchOptions) error {
+	refSpec := fmt.Sprintf("%s:%s", c.remoteHeadRef(), c.localHeadRef())
+	return fetch(gitCmd, originRemoteName, refSpec, fetchOpts)
 }
