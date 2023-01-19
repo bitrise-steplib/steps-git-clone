b192e9ed36f84dbbf81c1e493ee66f1bfe5c0b4e
vikas <vikas.shah@bitrise.io>
2023-01-19 10:30:07 -0600

    Move most important method to top of file. Rename Execute -> CheckoutState.

--- gitclone/gitclone.go
+++ gitclone/gitclone.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/bitrise-io/go-steputils/v2/stepconf"
 	"github.com/bitrise-io/go-utils/command/git"
-	"github.com/bitrise-io/go-utils/v2/command"
-	"github.com/bitrise-io/go-utils/v2/log"
+)
+
+const (
+	trimEnding               = "..."
+	originRemoteName         = "origin"
+	forkRemoteName           = "fork"
+	updateSubmoduleFailedTag = "update_submodule_failed"
+	sparseCheckoutFailedTag  = "sparse_checkout_failed"
 )
 
 // Config is the git clone step configuration
@@ -35,124 +40,11 @@ type Config struct {
 	BuildAPIToken   string `env:"build_api_token"`
 }
 
-const (
-	trimEnding               = "..."
-	originRemoteName         = "origin"
-	forkRemoteName           = "fork"
-	updateSubmoduleFailedTag = "update_submodule_failed"
-	sparseCheckoutFailedTag  = "sparse_checkout_failed"
-)
-
-type GitCloneStep struct {
-	logger      log.Logger
-	tracker     StepTracker
-	inputParser stepconf.InputParser
-	cmdFactory  command.Factory
+type GitCloner struct {
 }
 
-func NewGitCloneStep(logger log.Logger, tracker StepTracker, inputParser stepconf.InputParser, cmdFactory command.Factory) GitCloneStep {
-	return GitCloneStep{
-		logger:      logger,
-		tracker:     tracker,
-		inputParser: inputParser,
-		cmdFactory:  cmdFactory,
-	}
-}
-
-func (g GitCloneStep) ProcessConfig() (Config, error) {
-	var cfg Config
-	if err := g.inputParser.Parse(&cfg); err != nil {
-		return Config{}, fmt.Errorf("Error: %s\n", err)
-	}
-	stepconf.Print(cfg)
-
-	return cfg, nil
-}
-
-func (g GitCloneStep) checkoutState(gitCmd git.Git, cfg Config, patch patchSource) (strategy checkoutStrategy, isPR bool, err error) {
-	checkoutStartTime := time.Now()
-	checkoutMethod, diffFile := selectCheckoutMethod(cfg, patch)
-
-	fetchOpts := selectFetchOptions(checkoutMethod, cfg.CloneDepth, cfg.FetchTags, cfg.UpdateSubmodules, len(cfg.SparseDirectories) != 0)
-
-	checkoutStrategy, err := createCheckoutStrategy(checkoutMethod, cfg, diffFile)
-	if err != nil {
-		return nil, false, err
-	}
-	if checkoutStrategy == nil {
-		return nil, false, fmt.Errorf("failed to select a checkout stategy")
-	}
-
-	if err := checkoutStrategy.do(gitCmd, fetchOpts, selectFallbacks(checkoutMethod, fetchOpts)); err != nil {
-		g.logger.Infof("Checkout strategy used: %T", checkoutStrategy)
-		return nil, false, err
-	}
-
-	checkoutDuration := time.Since(checkoutStartTime).Round(time.Second)
-	g.logger.Println()
-	g.logger.Infof("Fetch and checkout took %s", checkoutDuration)
-	g.tracker.logCheckout(checkoutDuration, checkoutMethod, cfg.RepositoryURL)
-
-	return checkoutStrategy, isPRCheckout(checkoutMethod), nil
-}
-
-func updateSubmodules(gitCmd git.Git, cfg Config) error {
-	var opts []string
-	opts = append(opts, jobsFlag)
-
-	if cfg.SubmoduleUpdateDepth > 0 {
-		opts = append(opts, fmt.Sprintf("--depth=%d", cfg.SubmoduleUpdateDepth))
-	}
-
-	if err := runner.Run(gitCmd.SubmoduleUpdate(opts...)); err != nil {
-		return newStepError(
-			updateSubmoduleFailedTag,
-			fmt.Errorf("submodule update: %v", err),
-			"Updating submodules has failed",
-		)
-	}
-
-	return nil
-}
-
-func setupSparseCheckout(gitCmd git.Git, sparseDirectories []string) error {
-	if len(sparseDirectories) == 0 {
-		return nil
-	}
-
-	initCommand := gitCmd.SparseCheckoutInit(true)
-	if err := runner.Run(initCommand); err != nil {
-		return newStepError(
-			sparseCheckoutFailedTag,
-			fmt.Errorf("initializing sparse-checkout config failed: %v", err),
-			"Initializing sparse-checkout config has failed",
-		)
-	}
-
-	sparseSetCommand := gitCmd.SparseCheckoutSet(sparseDirectories...)
-	if err := runner.Run(sparseSetCommand); err != nil {
-		return newStepError(
-			sparseCheckoutFailedTag,
-			fmt.Errorf("updating sparse-checkout config failed: %v", err),
-			"Updating sparse-checkout config has failed",
-		)
-	}
-
-	// Enable partial clone support for the remote
-	sparseConfigCmd := gitCmd.Config("extensions.partialClone", originRemoteName, "--local")
-	if err := runner.Run(sparseConfigCmd); err != nil {
-		return newStepError(
-			sparseCheckoutFailedTag,
-			fmt.Errorf("enable partial clone support for the remote has failed: %v", err),
-			"Enable partial clone support for the remote has failed",
-		)
-	}
-
-	return nil
-}
-
-// Execute is the entry point of the git clone process
-func (g GitCloneStep) Execute(cfg Config) error {
+// CheckoutState is the entry point of the git clone process
+func CheckoutState(cfg Config) error {
 	defer g.tracker.wait()
 
 	gitCmd, err := git.New(cfg.CloneIntoDir)
@@ -241,9 +133,91 @@ Try using the env vars based on the webhook contents instead, such as $BITRISE_G
 		return nil
 	}
 
-	exporter := newOutputExporter(g.logger, g.cmdFactory, gitCmd)
+	exporter := newOutputExporter(logger, cmdFactory, gitCmd)
 	if err := exporter.exportCommitInfo(ref, isPR); err != nil {
 		return newStepError("export_envs_failed", err, "Exporting envs failed")
+	}
+
+	return nil
+}
+
+func checkoutState(gitCmd git.Git, cfg Config, patch patchSource) (strategy checkoutStrategy, isPR bool, err error) {
+	checkoutStartTime := time.Now()
+	checkoutMethod, diffFile := selectCheckoutMethod(cfg, patch)
+
+	fetchOpts := selectFetchOptions(checkoutMethod, cfg.CloneDepth, cfg.FetchTags, cfg.UpdateSubmodules, len(cfg.SparseDirectories) != 0)
+
+	checkoutStrategy, err := createCheckoutStrategy(checkoutMethod, cfg, diffFile)
+	if err != nil {
+		return nil, false, err
+	}
+	if checkoutStrategy == nil {
+		return nil, false, fmt.Errorf("failed to select a checkout stategy")
+	}
+
+	if err := checkoutStrategy.do(gitCmd, fetchOpts, selectFallbacks(checkoutMethod, fetchOpts)); err != nil {
+		g.logger.Infof("Checkout strategy used: %T", checkoutStrategy)
+		return nil, false, err
+	}
+
+	checkoutDuration := time.Since(checkoutStartTime).Round(time.Second)
+	g.logger.Println()
+	g.logger.Infof("Fetch and checkout took %s", checkoutDuration)
+	g.tracker.logCheckout(checkoutDuration, checkoutMethod, cfg.RepositoryURL)
+
+	return checkoutStrategy, isPRCheckout(checkoutMethod), nil
+}
+
+func updateSubmodules(gitCmd git.Git, cfg Config) error {
+	var opts []string
+	opts = append(opts, jobsFlag)
+
+	if cfg.SubmoduleUpdateDepth > 0 {
+		opts = append(opts, fmt.Sprintf("--depth=%d", cfg.SubmoduleUpdateDepth))
+	}
+
+	if err := runner.Run(gitCmd.SubmoduleUpdate(opts...)); err != nil {
+		return newStepError(
+			updateSubmoduleFailedTag,
+			fmt.Errorf("submodule update: %v", err),
+			"Updating submodules has failed",
+		)
+	}
+
+	return nil
+}
+
+func setupSparseCheckout(gitCmd git.Git, sparseDirectories []string) error {
+	if len(sparseDirectories) == 0 {
+		return nil
+	}
+
+	initCommand := gitCmd.SparseCheckoutInit(true)
+	if err := runner.Run(initCommand); err != nil {
+		return newStepError(
+			sparseCheckoutFailedTag,
+			fmt.Errorf("initializing sparse-checkout config failed: %v", err),
+			"Initializing sparse-checkout config has failed",
+		)
+	}
+
+	sparseSetCommand := gitCmd.SparseCheckoutSet(sparseDirectories...)
+	if err := runner.Run(sparseSetCommand); err != nil {
+		return newStepError(
+			sparseCheckoutFailedTag,
+			fmt.Errorf("updating sparse-checkout config failed: %v", err),
+			"Updating sparse-checkout config has failed",
+		)
+	}
+
+	// Enable partial clone support for the remote
+	sparseConfigCmd := gitCmd.Config("extensions.partialClone", originRemoteName, "--local")
+	if err := runner.Run(sparseConfigCmd); err != nil {
+		return newStepError(
+			sparseCheckoutFailedTag,
+			fmt.Errorf("enable partial clone support for the remote has failed: %v", err),
+			"Enable partial clone support for the remote has failed",
+		)
 	}
 
 	return nil
